a779084506775d75fd02800317b6b4aac1b6e81d
Samuel Puček <samuel.pucek@avast.com>
2022-11-23 21:09:21 +0100

    add tests

---
+++ tests/test_card.py
@@ -0,0 +1,36 @@
+import unittest
+from parameterized import parameterized
+
+from src.utils import Card
+
+
+class TestCard(unittest.TestCase):
+    @parameterized.expand(
+        [
+            ("2", Card(0, 0), False),
+            ("8", Card(0, 6), False),
+            ("Q", Card(0, 10), False),
+            ("A", Card(0, 12), True),
+            ("A", Card(3, 12), True),
+        ]
+    )
+    def test_is_ace_card(self, name: str, card: Card, expected_ace: bool):
+        self.assertEqual(card.is_ace_card(), expected_ace)
+
+    @parameterized.expand(
+        [
+            ("2", Card(0, 0), False, 2),
+            ("8", Card(0, 6), False, 8),
+            ("Q", Card(0, 10), False, 10),
+            ("A", Card(0, 12), False, 11),
+            ("A", Card(3, 12), True, 1),
+        ]
+    )
+    def test_card_value(
+        self, name: str, card: Card, ace_as_one, expected_value: int
+    ):
+        self.assertEqual(card.card_value(ace_as_one), expected_value)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -13,7 +13,9 @@ class TestHand(unittest.TestCase):
             ("four aces", [Card(i, 12) for i in range(4)], 14),
         ]
     )
-    def test_hand_value_aces(self, name, cards, expected_hand_value):
+    def test_hand_value_aces(
+        self, name: str, cards: list, expected_hand_value: int
+    ):
         hand = Hand(cards)
         self.assertEqual(hand.hand_value(), expected_hand_value)
 
@@ -47,7 +49,9 @@ class TestHand(unittest.TestCase):
             ),
         ]
     )
-    def test_hand_value_tricky_hand(self, name, cards, expected_hand_value):
+    def test_hand_value_tricky_hand(
+        self, name: str, cards: list, expected_hand_value: int
+    ):
         hand = Hand(cards)
         self.assertEqual(hand.hand_value(), expected_hand_value)
 
@@ -84,9 +88,58 @@ class TestHand(unittest.TestCase):
             ("6 Q K", [Card(0, 4), Card(0, 10), Card(0, 11)], True),
         ]
     )
-    def test_busted_hand(self, name, cards, expected_busted):
+    def test_busted_hand(self, name: str, cards: list, expected_busted: bool):
         hand = Hand(cards)
         self.assertEqual(hand.busted(), expected_busted)
+
+    @parameterized.expand(
+        [
+            ("2", [Card(0, 0)], False),
+            ("2 A", [Card(0, 0), Card(0, 12)], False),
+            ("2 A 3", [Card(0, 0), Card(0, 12), Card(0, 1)], False),
+            ("K A A", [Card(0, 11), Card(0, 12), Card(0, 12)], False),
+            ("A Q", [Card(3, 12), Card(2, 10)], True),
+            ("K A", [Card(1, 11), Card(3, 12)], True),
+            ("10 A", [Card(0, 8), Card(0, 12)], True),
+            (
+                "K K A A",
+                [Card(0, 11), Card(0, 11), Card(0, 12), Card(0, 12)],
+                False,
+            ),
+        ]
+    )
+    def test_black_jack_hand(
+        self, name: str, cards: list, expected_black_jack: bool
+    ):
+        hand = Hand(cards)
+        self.assertEqual(hand.black_jack(), expected_black_jack)
+
+    @parameterized.expand(
+        [
+            ("2", [Card(0, 0)], False),
+            ("2 A", [Card(0, 0), Card(0, 12)], False),
+            ("2 A 3", [Card(0, 0), Card(0, 12), Card(0, 1)], False),
+            ("K A A", [Card(0, 11), Card(0, 12), Card(0, 12)], False),
+            ("A Q", [Card(3, 12), Card(2, 10)], False),
+            ("K A", [Card(1, 11), Card(3, 12)], False),
+            ("2 2", [Card(1, 0), Card(3, 0)], True),
+            ("3 3", [Card(1, 1), Card(3, 1)], True),
+            ("4 4", [Card(1, 2), Card(3, 2)], True),
+            ("5 5", [Card(1, 3), Card(3, 3)], True),
+            ("6 6", [Card(1, 4), Card(3, 4)], True),
+            ("7 7", [Card(1, 5), Card(3, 5)], True),
+            ("8 8", [Card(1, 6), Card(3, 6)], True),
+            ("9 9", [Card(1, 7), Card(3, 7)], True),
+            ("10 10", [Card(1, 8), Card(3, 8)], True),
+            ("J J", [Card(1, 9), Card(3, 9)], True),
+            ("Q Q", [Card(1, 10), Card(3, 10)], True),
+            ("K K", [Card(1, 11), Card(3, 11)], True),
+            ("A A", [Card(1, 12), Card(3, 12)], True),
+        ]
+    )
+    def test_hand_of_pairs(self, name: str, cards: list, expected_pairs: bool):
+        hand = Hand(cards)
+        self.assertEqual(hand.hand_of_pairs(), expected_pairs)
 
 
 if __name__ == "__main__":
